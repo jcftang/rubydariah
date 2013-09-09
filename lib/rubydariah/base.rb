0a4689f230c4ebfc566cc9a6a341739718dede1a
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-09-09 21:12:37 +0100

    include httparty in class

--- lib/rubydariah/base.rb
+++ lib/rubydariah/base.rb
@@ -1,6 +1,9 @@
 module Rubydariah
   module Storage
     class Base
+      include HTTParty
+      base_uri 'dariah.de'
+
       def initialize(username, password)
           @auth = {:username => username, :password => password}
       end
