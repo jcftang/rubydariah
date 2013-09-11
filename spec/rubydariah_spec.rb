ae013ba7df8547b1f33b43237389bffbceac1998
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-09-11 16:50:48 +0100

    rename @auth to @dariah which is more meaningful

--- spec/rubydariah_spec.rb
+++ spec/rubydariah_spec.rb
@@ -10,21 +10,21 @@ describe Rubydariah::Storage do
 #  end
 
   before do
-    @auth = Rubydariah::Storage.new("http://ipedariah1.lsdf.kit.edu:8080/StorageImplementation-1.0-SNAPSHOT", "foo", "bar")
+    @dariah = Rubydariah::Storage.new("http://ipedariah1.lsdf.kit.edu:8080/StorageImplementation-1.0-SNAPSHOT", "foo", "bar")
   end
 
   it "should create a new object" do
-    @auth.should be_kind_of Rubydariah::Storage
+    @dariah.should be_kind_of Rubydariah::Storage
   end
 
   it "should post a file" do
     VCR.use_cassette 'post' do
       file_location = File.expand_path(File.dirname(__FILE__) + '/fixtures/samplefile.mp3')
       file_data = File.read(file_location)
-      status, pid = @auth.post(file_data, 'audio/mpeg')
+      status, pid = @dariah.post(file_data, 'audio/mpeg')
       status.should == 201
 
-      status = @auth.delete(pid)
+      status = @dariah.delete(pid)
       status.should == 204
     end
   end
@@ -35,13 +35,13 @@ describe Rubydariah::Storage do
       file_data = File.read(file_location)
 
       md5 = Digest::MD5.hexdigest(file_data)
-      status, pid = @auth.post(file_data, 'audio/mpeg')
+      status, pid = @dariah.post(file_data, 'audio/mpeg')
       status.should == 201
 
-      status, put_pid = @auth.put(pid, file_data, 'audio/mpeg')
+      status, put_pid = @dariah.put(pid, file_data, 'audio/mpeg')
       put_pid.should == pid
 
-      status, data = @auth.get(put_pid)
+      status, data = @dariah.get(put_pid)
       put_md5 = Digest::MD5.hexdigest(data)
       put_md5.should == md5
     end
@@ -52,19 +52,19 @@ describe Rubydariah::Storage do
       file_location = File.expand_path(File.dirname(__FILE__) + '/fixtures/samplefile.mp3')
       file_data = File.read(file_location)
       md5 = Digest::MD5.hexdigest(file_data)
-      status, pid = @auth.post(file_data, 'audio/mpeg')
+      status, pid = @dariah.post(file_data, 'audio/mpeg')
       status.should == 201
 
-      status, content_type = @auth.head(pid)
+      status, content_type = @dariah.head(pid)
       status.should == 200
       content_type.should eq('audio/mpeg')
 
-      status, data = @auth.get(pid)
+      status, data = @dariah.get(pid)
       status.should == 200
       get_md5 = Digest::MD5.hexdigest(data)
       get_md5.should == md5
 
-      status = @auth.delete(pid)
+      status = @dariah.delete(pid)
       status.should == 204
     end
   end
@@ -73,27 +73,27 @@ describe Rubydariah::Storage do
     VCR.use_cassette 'head' do
       file_location = File.expand_path(File.dirname(__FILE__) + '/fixtures/samplefile.mp3')
       file_data = File.read(file_location)
-      status, pid = @auth.post(file_data, 'audio/mpeg')
+      status, pid = @dariah.post(file_data, 'audio/mpeg')
       status.should == 201
 
-      status, content_type = @auth.head(pid)
+      status, content_type = @dariah.head(pid)
       status.should == 200
       content_type.should eq('audio/mpeg')
 
-      status = @auth.delete(pid)
+      status = @dariah.delete(pid)
       status.should == 204
     end
   end
 
   it "should raise an error if an exception is caught" do
     VCR.use_cassette 'get_exception' do
-      expect { @auth.get('test') }.to raise_error(Rubydariah::DariahError)
+      expect { @dariah.get('test') }.to raise_error(Rubydariah::DariahError)
     end
   end
 
   it "should get a list of options in the headers" do
     VCR.use_cassette 'options' do
-      status, allowed = @auth.options
+      status, allowed = @dariah.options
       status.should == 200
       allowed.should include "OPTIONS"
       allowed.should include "GET"
@@ -108,10 +108,10 @@ describe Rubydariah::Storage do
     VCR.use_cassette 'delete' do
       file_location = File.expand_path(File.dirname(__FILE__) + '/fixtures/samplefile.mp3')
       file_data = File.read(file_location)
-      status, pid = @auth.post(file_data, 'audio/mpeg')
+      status, pid = @dariah.post(file_data, 'audio/mpeg')
       status.should == 201
 
-      status = @auth.delete(pid)
+      status = @dariah.delete(pid)
       status.should == 204
     end
   end
